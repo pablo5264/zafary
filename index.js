9c62d6232ee893deedaa27044ab518dc86a614b4
pablo5264 <pablo5264@gmail.com>
2019-12-22 21:15:48 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const boton = window.document.querySelector('#boton');
+
+boton.addEventListener('click', mostrarDatos); 
+
+function mostrarDatos(){
+    const input1= window.document.querySelector('#email');
+    const input2 = window.document.querySelector('#password');
+    const textoInput = input1.value;
+    const textoInput2 = input2.value; 
+
+    input1.value = '';
+    input1.focus(); 
+
+    input2.value = '';
+    input2.focus(); 
+
+   
+    if (textoInput === 'ejemplo@gmail.com' && textoInput2 === '123456'){
+        console.log('Iniciando sesión...')    
+    } 
+    else {
+        console.log('Email y/o contraseña incorrectos')
+    }
+
+   
+
+}
